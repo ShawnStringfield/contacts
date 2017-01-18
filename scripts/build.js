0929535be3c6edd2f1ebd0673dbe99c34b730ec8
Shawn Stringfield <shawn.stringfield@gmail.com>
2017-01-17 21:21:26 -0500

    removed build step from procfile

--- scripts/build.js
+++ scripts/build.js
@@ -12,6 +12,8 @@ process.env.NODE_ENV = 'production';
 // Load environment variables from .env file.
 require('dotenv').config({silent: true});
 
+console.log( 'ran' );
+
 // Ensures that a directory is empty. Deletes directory contents if the directory is not empty. If the directory does not exist, it is created. The directory itself is not deleted.
 fs.emptyDirSync(paths.appBuild);
 
